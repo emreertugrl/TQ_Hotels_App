0c3369b6b09d3f6b7eb3fd7fef4b88b9ed45eb4e
emreertugrl <emrertugrl@gmail.com>
2024-11-20 21:58:38 +0300

    Home components render

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,5 +1,19 @@
+import Container from "../../components/container";
+import Filter from "./Filter";
+import Hero from "./Hero";
+import List from "./List";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container design="my-5">
+      <Hero />
+
+      <div className="grid grid-cols-1 lg:grid-cols-4 max-lg:mt-10 gap-5">
+        <Filter />
+        <List />
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
