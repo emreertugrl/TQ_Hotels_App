50de16505e5ddc040dd000ab3b95a15eb6ae33af
emreertugrl <emrertugrl@gmail.com>
2024-11-20 21:36:43 +0300

    installations completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Detail from "./pages/detail/index";
+import Home from "./pages/home/index";
+import Create from "./pages/create/index";
+import Header from "./components/header";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/place/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
